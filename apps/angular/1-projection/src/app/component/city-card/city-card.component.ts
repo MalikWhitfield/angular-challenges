a597b8d82230aaa6d58145dc50104f15fe1f9e34
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-20 17:03:50 -0400

    feat(card): implement card projection

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import { FakeHttpService } from '../../data-access/fake-http.service';
+import { CardType } from '../../model/card.model';
+import { City } from '../../model/city.model';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      [list]="cities"
+      [type]="cardType"
+      customClass="bg-light-green"></app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
+  cardType = CardType.CITY;
 
-  ngOnInit(): void {}
+  constructor(
+    private fakeHttpService: FakeHttpService,
+    private store: CityStore,
+  ) {}
+
+  ngOnInit(): void {
+    this.fakeHttpService.fetchCities$.subscribe((s) => this.store.addAll(s));
+
+    this.store.cities$.subscribe((res) => {
+      this.cities = res;
+      console.log(this.cities);
+    });
+  }
 }
