11487416540261d1a9d687fff1f75a40c193424d
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-21 21:26:54 -0400

    feat(purePipe): implement custom pipe

--- apps/angular/8-pure-pipe/src/app/app.component.ts
+++ apps/angular/8-pure-pipe/src/app/app.component.ts
@@ -1,21 +1,16 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { PersonPipe } from '../pipes/personPipe.pipe';
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, PersonPipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
+      {{ person | personPipe: index }}
     </div>
   `,
 })
 export class AppComponent {
   persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
 }
