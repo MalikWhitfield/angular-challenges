caa18659604c0bc79cc7afe48fcf8c324b99e7ca
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-23 15:44:49 -0400

    feat(lazyLoad): implement lazy loading standalone components

--- apps/angular/52-lazy-load-component/src/main.ts
+++ apps/angular/52-lazy-load-component/src/main.ts
@@ -1,6 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { ApplicationConfig } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+export const appConfig: ApplicationConfig = {
+  providers: [],
+};
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
