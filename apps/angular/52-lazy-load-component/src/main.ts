3d33b10aa4df2d700a0b9a3952ed2d1607ebadbf
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-23 15:46:17 -0400

    feat(lazyLoad): use appConfig correctly

--- apps/angular/52-lazy-load-component/src/main.ts
+++ apps/angular/52-lazy-load-component/src/main.ts
@@ -1,10 +1,6 @@
-import { ApplicationConfig } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-
-export const appConfig: ApplicationConfig = {
-  providers: [],
-};
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
